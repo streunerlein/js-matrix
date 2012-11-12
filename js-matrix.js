fbfca1d24ffa6eca820da6cc129e90204cd80fb3
Dominique Sandoz <dominique.sandoz@gmail.com>
2012-11-12 18:19:00 +0100

    Makes it awesome...

--- js-matrix.js
+++ js-matrix.js
@@ -1,46 +1,52 @@
 
 var jshint = require("jshint").JSHINT;
 
-var charnumber = 6;
-
-// only ascii stuff
-var maxcharcode = 127;
-var mincharcode = 32;
-
-var firstchar = String.fromCharCode(mincharcode);
-
-var str = strpad("", charnumber, firstchar);
-
-var num = 0;
-var i = 0;
-var passed = 0;
-var t = false;
-var starttime = new Date().getTime(), endtime = 0, persec = 0;
+var charnumber = process.argv[2];
 
 while (true) {
-	// increment word
-	for (i = charnumber-1; i > -1; i--) {
-		var cc = str.charCodeAt(i);
+	run();
+	if (process.argv.length > 3 && process.argv[3] == 'continue') {
+		charnumber++;
+		continue;
+	}
+	break;
+}
 
-		if (cc < maxcharcode) {
-			str = replaceAt(str, i, String.fromCharCode(cc + 1));
+function run() {
+	// only ascii stuff
+	var maxcharcode = 127;
+	var mincharcode = 32;
+
+	var firstchar = String.fromCharCode(mincharcode);
+
+	var str = strpad("", charnumber, firstchar);
+
+	var num = 0;
+	var i = 0;
+	var starttime = new Date().getTime(), endtime = 0, persec = 0;
+
+	while (true) {
+		// increment word
+		for (i = charnumber-1; i > -1; i--) {
+			var cc = str.charCodeAt(i);
+
+			if (cc < maxcharcode) {
+				str = replaceAt(str, i, String.fromCharCode(cc + 1));
+				break;
+			}
+			else {
+				str = replaceAt(str, i, firstchar);
+			}
+		}
+
+		if (i === 0) {
 			break;
 		}
-		else {
-			str = replaceAt(str, i, firstchar);
+
+		if (jshint(str)) {
+			console.log(str);
 		}
 	}
-
-	if (i === 0) {
-		console.log("Done");
-		break;
-	}
-
-	if (jshint(str)) {
-		passed++;
-	}
-
-	console.log(str);
 }
 
 function strpad(str, length, padder) {
